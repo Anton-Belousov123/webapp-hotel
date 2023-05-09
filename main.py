dbefd699a4a2c208166a3b3e9c4eeb88b477a17d
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-09 17:19:50 +0300

    еуые

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 from utils import is_html_file_exists
 from OpenSSL import SSL
+
 context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
 context.use_privatekey_file('server.key')
 context.use_certificate_file('server.crt')
-
 
 app = Flask(__name__)
 
@@ -50,5 +50,4 @@ def admin_page():
     print(request.form.to_dict())
 
 
-
-app.run(debug=True, host='0.0.0.0', port='5000')
+app.run(debug=True, host='0.0.0.0', port='5000', ssl_context=context)
