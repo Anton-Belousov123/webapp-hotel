42708c610f38a103a1ae74f8f67e75fedcc223f6
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-16 01:43:26 +0300

    v 1.0.0

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import random
 
+import requests
 import telebot
 from aiogram.types import InputMessageContent
 from telebot.types import InlineQueryResultArticle, InputTextMessageContent
+
 
 def send_answer(data):
     try:
         API_TOKEN = '5750853621:AAGifNbBkrsfjs-pddUmcSwmdyzTxaGSYXA'
         bot = telebot.TeleBot(API_TOKEN, parse_mode="HTML")
         text = f'Я хочу заказать {data["title"]} стоимостью {data["price"]}.'
+        requests.post()
         bot.answer_web_app_query(
             web_app_query_id=data['query_id'],
             result=InlineQueryResultArticle(
--- tg.py
+++ tg.py
@@ -1,25 +1,37 @@
+import time
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 
-bot = Bot(token='6215708830:AAGKSA7XfghK_TMBu3obT8-Vs7dbyk5Or2s')
+bot = Bot(token='5750853621:AAGifNbBkrsfjs-pddUmcSwmdyzTxaGSYXA')
 
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
+async def h_c(m: types.message):
+    keyword = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyword.add(types.KeyboardButton('Зарегистрироваться в WebApp'))
+    await m.answer('Привет мой друг!', reply_markup=keyword)
+
+
+@dp.message_handler(lambda m: m.text == 'Зарегистрироваться в WebApp')
 async def start_command(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://hotel-bot.site/')))
-    await message.answer('Привет мой друг!', reply_markup=keyboard)
+    await message.answer('Открыть WebApp!', reply_markup=keyboard)
 
 
 @dp.message_handler()
 async def gr(message: types.Message):
     if 'Я хочу заказать ' in message.text:
         order = message.text.replace('Я хочу заказать ', '')
+
+        time.sleep(3)
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(
             types.InlineKeyboardButton('Подтвердить', url='https://api.whatsapp.com/send?phone=905398266855&text=Здравствуйте! Мне нужна информация о недвижимости'))
-        await message.answer(f"Спасибо за ваш заказ!\nВы заказали {order}.\nДля подтверждения заказа нажмите на кнопку в этом сообщении", reply_markup=keyboard)
+        await message.answer(f"Спасибо за ваш заказ!\nДля подтверждения заказа нажмите на кнопку в этом сообщении", reply_markup=keyboard)
 
 
 executor.start_polling(dp)
