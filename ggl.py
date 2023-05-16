e536d9f23a703fd2bd62ee29300c55a83f80a975
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-16 23:01:17 +0300

    v 1.0.0

--- ggl.py
+++ ggl.py
@@ -2,11 +2,19 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def read_message_preview():
+def auth():
     scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
 
     creds = ServiceAccountCredentials.from_json_keyfile_name('misc/chat-384709-f24e97dc0a0b.json', scope)
     client = gspread.authorize(creds)
+    return client
+
+
+def read_message_preview():
     sheet = client.open('ЧАТ').sheet1
-    rules = sheet.cell(10, 2).value
+    rules = sheet.cell(11, 2).value
+    print(rules)
     return rules
+
+
+client = auth()
--- tg.py
+++ tg.py
@@ -11,13 +11,6 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
-async def h_c(m: types.message):
-    keyword = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyword.add(types.KeyboardButton('Зарегистрироваться в WebApp'))
-    await m.answer('Привет мой друг!', reply_markup=keyword)
-
-
-@dp.message_handler(lambda m: m.text == 'Зарегистрироваться в WebApp')
 async def start_command(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://hotel-bot.site/')))
@@ -29,12 +22,14 @@ async def gr(message: types.Message):
     if 'Я хочу заказать ' in message.text:
         order = message.text.replace('Я хочу заказать ', '')
         print(message)
-        amo.make_order(f"{message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}", order)
+        print(order)
+        amo.make_order(f"{message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}",
+                       order)
         keyboard = types.InlineKeyboardMarkup()
         keyboard.add(
-            types.InlineKeyboardButton('Подтвердить', url=f'https://api.whatsapp.com/send?phone={ggl.read_message_preview().replace("!order!", order)}'))
+            types.InlineKeyboardButton('Подтвердить',
+                                       url=f'https://api.whatsapp.com/send?phone={ggl.read_message_preview().replace("!order!", order)}'))
         await message.answer(f"Спасибо за ваш заказ! ({order})", reply_markup=keyboard)
 
 
 executor.start_polling(dp)
-
